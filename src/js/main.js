e7fc617c6e4c2b5f979d8c7d18d3d2150f6dea99
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-12-27 14:25:36 +0800

    Show attribution when certain button is pressed
    Only buttons with class `mock-btn` will trigger this

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 const ATTRIBUTION_BG_ID = "attribution-bg";
 const HIDDEN_ATTRIBUTION_CLASS = "attribution--hidden";
+const MOCK_BUTTON_CLASS = "mock-btn";
 
 let attribution_bg = document.getElementById(ATTRIBUTION_BG_ID);
-
 attribution_bg.onclick = () => { attribution_bg.classList.add(HIDDEN_ATTRIBUTION_CLASS); }
+
+function showAttribution() {
+	attribution_bg.classList.remove(HIDDEN_ATTRIBUTION_CLASS);
+	console.log("Showing Attr...")
+}
+
+for (let btn of document.getElementsByClassName(MOCK_BUTTON_CLASS)) {
+	btn.onclick = showAttribution;
+}
