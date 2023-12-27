cd7d82f61aec43e928ff636032d018024f7ef140
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-12-27 18:10:18 +0800

    Minor JS changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,15 @@ const ATTRIBUTION_BG_ID = "attribution-bg";
 const HIDDEN_ATTRIBUTION_CLASS = "attribution--hidden";
 const MOCK_BUTTON_CLASS = "mock-btn";
 
-let attribution_bg = document.getElementById(ATTRIBUTION_BG_ID);
-attribution_bg.onclick = () => { attribution_bg.classList.add(HIDDEN_ATTRIBUTION_CLASS); }
+let attributionBG = document.getElementById(ATTRIBUTION_BG_ID);
+attributionBG.onclick = hideAttribution;
 
 function showAttribution() {
-	attribution_bg.classList.remove(HIDDEN_ATTRIBUTION_CLASS);
-	console.log("Showing Attr...")
+	attributionBG.classList.remove(HIDDEN_ATTRIBUTION_CLASS);
+}
+
+function hideAttribution() {
+	attributionBG.classList.add(HIDDEN_ATTRIBUTION_CLASS);
 }
 
 for (let btn of document.getElementsByClassName(MOCK_BUTTON_CLASS)) {
