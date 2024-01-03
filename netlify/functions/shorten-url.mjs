836ed54c001af25970c194bc11a85e654228ddeb
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-01-03 14:27:31 +0800

    Finished Netlify Function to request cleanuri.com API

--- netlify/functions/shorten-url.mjs
+++ netlify/functions/shorten-url.mjs
@@ -1,3 +1,25 @@
-export default async = (req, context) => {
+const SHORTEN_ENDPOINT = "https://cleanuri.com/api/v1/shorten";
 
+export default async (req, context) => {
+	const requestedURL = await streamToString(req.body);
+
+	const response = await fetch(SHORTEN_ENDPOINT, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		body: requestedURL
+	});
+
+	return await response.json();
+}
+
+async function streamToString(stream) {
+    const chunks = [];
+
+    for await (const chunk of stream) {
+        chunks.push(Buffer.from(chunk));
+    }
+
+    return Buffer.concat(chunks).toString("utf-8");
 }
