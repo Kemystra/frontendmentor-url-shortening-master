82b395b15842aed1c53b20052226eaa3deeef8cd
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-01-13 17:49:55 +0800

    Made shortener form to use React

--- src/jsx/shortener.jsx
+++ src/jsx/shortener.jsx
@@ -1,6 +1,3 @@
-const SHORTENER_FORM_ID = "shortener-form";
-const SHORTENER_URL_INPUT_ID = "url-shortener-input";
-
 const RESULT_PANEL_CLASS = "shortener-result-panel";
 const RESULT_PANEL_WAIT_CLASS = "shortener-result-panel--wait";
 
@@ -9,54 +6,47 @@ const RESULT_PANEL_WAIT_CLASS = "shortener-result-panel--wait";
 // Using Netlify Function to serve that
 const URL_SHORTENER_ENDPOINT = "/shorten-url";
 
-let shortenedURLs = [];
-let shortenerSection = document.getElementById(SHORTENER_SECTION_ID);
-let shortenerForm = document.getElementById(SHORTENER_FORM_ID);
-let shortenerURLInput = document.getElementById(SHORTENER_URL_INPUT_ID);
-
 class URLPair {
 	constructor(longURL) {
 		this.longURL = longURL;
-		this.shortenedURL = "";
-	}
-}
-
-shortenerForm.onsubmit = async e => {
-	e.preventDefault();
-
-	let originalURL = shortenerURLInput.value.trim();
-
-	let resultPanel = document.createElement("div");
-	resultPanel.classList.add(RESULT_PANEL_CLASS, RESULT_PANEL_WAIT_CLASS);
-	shortenerSection.appendChild(resultPanel);
-
-	try {
-		const response = await fetch(URL_SHORTENER_ENDPOINT, {
-			method: "POST",
-			body: `url=${originalURL}`
-		});
-		const result = await response.json();
-		alert(result);
-	}
-
-	catch(error) {
-		console.log(error);
 	}
 }
 
 const Shortener = () => {
-	return <p>You fool!</p>
-	/*
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		let originalURL = shortenerURLInput.value.trim();
+
+		try {
+			const response = await fetch(URL_SHORTENER_ENDPOINT, {
+				method: "POST",
+				body: `url=${originalURL}`
+			});
+			const result = await response.json();
+			alert(result);
+		}
+
+		catch(error) {
+			console.log(error);
+		}
+	}
+
 	return (
-		<form action="" class="shorten-section__form" id="shortener-form">
-			<div class="shorten-section__url-field url-field">
-				<input placeholder="Shorten a link here..." type="url" id="url-shortener-input" class="url-field__input" />
-				<p class="url-field__error-msg">Please add a link</p>
-			</div>
-			<button class="shorten-section__process-btn">Shorten It!</button>
-		</form>
+		<>
+			<form action="" className="shorten-section__form" onSubmit={handleSubmit}>
+				<div className="shorten-section__url-field url-field">
+					<input placeholder="Shorten a link here..." type="url" className="url-field__input" />
+					<p className="url-field__error-msg">Please add a link</p>
+				</div>
+				<button className="shorten-section__process-btn">Shorten It!</button>
+			</form>
+		</>
 	)
-	*/
+}
+
+const ResultPanel = () => {
+	return <div className={RESULT_PANEL_CLASS + " " + RESULT_PANEL_WAIT_CLASS}></div>
 }
 
 export default Shortener;
